6c0e5ca1fa58162dcfa71603f74d06d67813782a
brunobian <brunobian@gmail.com>
2014-03-06 09:35:20 -0800

    Paso el 3º texto al final

--- cloze/models.py
+++ cloze/models.py
@@ -330,10 +330,10 @@ class Subject(models.Model):
 		
 		# Si se quiere cambiar el orden de los textos o eliminar un texto, se cambia desde acá. 
 		# Cada elemento de seq_base1 es un experimento variable pero siempre del mismo texto. 
-		seq_base0 = seq_base1[2:]
+		seq_base0 = seq_base1[3:]
 		seq_base0.append(seq_base1[0]) # Pongo el primer texto al final
 		seq_base0.append(seq_base1[1]) # Pongo el segundo texto al final
-	
+		seq_base0.append(seq_base1[2]) # Pongo el segundo texto al final
 		
 		return (ts.id,seq_base0)       
 
