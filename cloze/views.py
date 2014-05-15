88ccd565ce2144751efa36c72b1f8a28ee0a44a7
brunobian <brunobian@gmail.com>
2014-05-15 16:40:26 -0300

    asd

--- cloze/views.py
+++ cloze/views.py
@@ -286,9 +286,3 @@ def bajar_sujetos(request):
     response['Content-Disposition'] = 'attachment; filename=subject_log.csv'
     return response
 
-def server_error(request):
-    # one of the things ‘render’ does is add ‘STATIC_URL’ to
-    # the context, making it available from within the template.
-    response = render(request, "500.html")
-    response.status_code = 500
-    return response
