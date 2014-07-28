45967c64a596896bc751dbe9a3588eec513ea74f
root <root@calamaro.exp.dc.uba.ar>
2014-07-28 13:22:05 -0300

    Revert "sacando prints"
    This reverts commit 63d24d3c8b766aa9400bfe77635fb3662039e151.

--- cloze/views.py
+++ cloze/views.py
@@ -249,8 +249,8 @@ def bajar_todo(request):
         tebs = te.body.split()
                
         epoch = timezone.make_aware(datetime.datetime.utcfromtimestamp(0), timezone.get_default_timezone())
-        #print t.initial_time
-        #print epoch
+        print t.initial_time
+        print epoch
         delta = t.initial_time - epoch 
         ep = long(delta.total_seconds()*1000) + 3600 * 1000 * 3 # GMT-3 correction
         
